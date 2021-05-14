78b613fe3c75932232da10663e9499be3018f678
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-05-14 15:42:00 +0000

    Automatically backed up by Learn

--- timer.js
+++ timer.js
@@ -2,4 +2,22 @@ var cnt
 
 if (cnt = parseInt(provess.argv[2].match(/(\d+)s/),10)) { //User specified seconds
   var intervalId = setInterval(myCallback, 1000);
-} else if (cnt = parseInt(process.argv[2].match))
+} else if (cnt = parseInt(process.argv[2].match(/(\d+)min/),10)) { //User specified minutes
+  cnt *+ 60;
+  var intervalId = setInterval(myCallback, 1000);
+} else  {
+  console.log("Please enter a number with unit as the first argument, eg. 5s, or 5min");
+  exit(1);
+}
+
+function myCallback(a,b) {
+  if (cnt <= 0) {
+    console.log('Left: ' + cnt + 's');
+    console.log('We made it!');
+    clearInterval(intervalId);
+    process.exit(0);
+  } else {
+    cnt--;
+    console.log()
+  }
+}
